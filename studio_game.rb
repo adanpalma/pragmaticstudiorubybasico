fdc982e76cc8c9a83b81b49257d397e62d23199c
Adan Palma <adanpalma@hotmail.com>
2016-11-30 23:18:50 -0500

    Se añade attr_accesor and reader.  Se añade atributo virtual y se especializa metodo player_name= para que cambie el nombre a mayuscula

--- studio_game.rb
+++ studio_game.rb
@@ -1,59 +1,74 @@
 class Player
+  attr_reader   :player_health
+  attr_accessor :player_name
+  
   def initialize(player_name, player_health=100)
     @player_name = player_name.capitalize
     @player_health = player_health
   end
   
+  def player_name=(player_name)
+    @player_name = player_name.capitalize
+  end  
+  
   def to_s
-    "I' am #{@player_name} with health of #{@player_health}"
+    "I' am #{@player_name} with a health of #{@player_health} and a score of #{self.score}"
   end
   
   def w00t
     puts "#{@player_name} was w00ted!"
     @player_health += 15
   end
+  
   def blam
     puts "#{@player_name} was blammed!"
     @player_health -= 10
   end
+  
+  def score
+    @player_health + @player_name.length
+  end
+  
 end
-
-player1 = Player.new("moe",120)
-puts player1
-player1.w00t
-puts player1
-
-
 
 def time
   Time.new.strftime("%I:%M:%S")
 end
 
-def say_hello(player_name,player_health=100)
-   "I'am #{player_name.capitalize} with a health of #{player_health} as of #{time}"
-end
-
-
-puts time
-puts say_hello("larry",60)
-puts say_hello("curly",125)
-puts say_hello("moe")
-puts say_hello("shemp", 90)
-
-
-name1 = "larry"
-health1 = 60
-name2 = "curly"
-health2 = 125
-name3 = "moe"
-health3 = 100
-name4="shemp"
-health4=90
 time = Time.new
 puts "The game started on #{time.strftime("%A %d/%m/%Y at %I:%M %p")}"
-puts "#{name1.capitalize}'s health is #{health1}"
-puts "#{name2.upcase}'s health is #{health2}"
-puts "#{name3} has a health of #{health3}".center(50,".")
-puts "#{name4.capitalize.ljust(48,".")} #{health4} health"
+
+player1 = Player.new("moe",120)
+player2 = Player.new("larry",120)
+
+
+player1.w00t
+player2.blam
+
+puts player1.player_name
+puts player1.player_health
+puts player1.score
+
+
+puts player2.player_name
+puts player2.player_health
+puts player2.score
+
+player2.player_name="lawrence"
+puts player2.player_name
+
+puts player1
+puts player2
+
+
+
+
+
+
+
+#puts "#{name1.capitalize}'s health is #{health1}"
+#puts "#{name2.upcase}'s health is #{health2}"
+#puts "#{name3} has a health of #{health3}".center(50,".")
+#puts "#{name4.capitalize.ljust(48,".")} #{health4} health"
 
 
